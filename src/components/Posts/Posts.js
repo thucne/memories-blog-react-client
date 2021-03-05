204183b84571048616b3db52d8dad2e5252116d0
Thuc Katy <katyperrycbt@gmail.com>
2021-03-05 15:35:21 +0700

    UDD

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -14,7 +14,7 @@ const Posts = ({ setCurrentId }) => {
     return (
         !posts.length ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {posts.map((post) => (
+                {posts.sort((a, b) => {return (new Date(b.createdAt) - new Date(a.createdAt))}).map((post) => (
                     <Grid key={post._id} item xs={12} sm={6}>
                         <Post post={post} setCurrentId={setCurrentId}/>
                     </Grid>
